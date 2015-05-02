caa6e1b1af629601cee3ebee3e31235f5969d1cd
Yuta Okazaki <s04155yo@gmail.com>
2015-05-02 14:24:27 -0400

    Add sketch

---
+++ sketch.js
@@ -0,0 +1,8 @@
+// JS or Ruby. or Both.
+// to be parse the following to return a factorial function.
+//
+// 階乗っていうのは、
+// 数字を一つ使って、
+// もしその数字が0だったら1を返して、
+// それ以外だったら
+// 同じ事を一つ少ない数字でやった結果とその数字をかけたものを返すんだよ。
