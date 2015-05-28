efd951c741ac1d1a2c60b842935ecef823db5930
Yuta Okazaki <s04155yo@gmail.com>
2015-05-28 19:25:00 -0400

    more desc on sketch

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 // JS or Ruby. or Both.
-// to be parse the following to return a factorial function.
+// when parse the following Japanese sentence, it should return a factorial function.
 //
 // 階乗っていうのは、
 // 数字を一つ使って、
 // もしその数字が0だったら1を返して、
 // それ以外だったら
 // 同じ事を一つ少ない数字でやった結果とその数字をかけたものを返すんだよ。
+//
+//
+// the same thing in Scheme
+// (define (factorial n)
+//   (cond
+//     ((zero? n) 1)
+//     (else (* n (factorial (- n 1))))))
