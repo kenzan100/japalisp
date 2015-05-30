94ddab67c830ada326f73381d52586147499299c
Yuta Okazaki <s04155yo@gmail.com>
2015-05-29 20:23:55 -0400

    more lexer cases

--- lexer.rb
+++ lexer.rb
@@ -1,9 +1,48 @@
+require 'pp'
+require 'byebug'
 class Lexer
+
+  def scan_operator(chunk)
+    partial_tokens = []
+    if operands = chunk[/\s*(.+)(をかけた結果|をかけたもの)/,1]
+      partial_tokens << [:*, "かけた結果"]
+      identifier_operands = operands.split("と").map do |operand|
+        [:IDENTIFIER, operand]
+      end
+      partial_tokens += identifier_operands
+    end
+    partial_tokens
+  end
+
   def tokenize(code)
     code.chomp!
     tokens = []
-    if matched = code[/\A(.+)っていうのは/, 1]
-      tokens << [:DEF, "def"] << [:IDENTIFIER, matched]
+    i = 0 #current char position
+    while i < code.size
+      chunk = code[i..-1]
+      pp chunk
+      case
+      when def_matched = chunk[/\s*(.+)っていうのは/, 1]
+        tokens << [:DEF, "def"] << [:IDENTIFIER, def_matched]
+        i += chunk[/.+っていうのは/].size
+      when arg_matched = chunk[/\s*(.+)を使って/,1]
+        args = arg_matched.split("と")
+        identifier_args = args.map do |arg|
+          [:IDENTIFIER, arg]
+        end
+        tokens << [:ARGS, "arg"]
+        tokens += identifier_args
+        i += chunk[/.+を使って/].size
+      when return_matched = chunk[/\s*(.+)を返す/,1]
+        tokens << [:RETURN, "返す"]
+        tokens += scan_operator(return_matched)
+        i += chunk[/.+を返す.+$/].size
+      when chunk[/.+。$/]
+        tokens << [:TERM, "。"]
+        i += chunk[/.+。$/].size
+      else
+        i += chunk.size
+      end
     end
     tokens
   end
--- test/lexer_test.rb
+++ test/lexer_test.rb
@@ -1,8 +1,54 @@
 require 'minitest/autorun'
 require_relative "../lexer"
 
+# 階乗っていうのは、
+# 数字を一つ使って、
+# もしその数字が0だったら1を返して、
+# それ以外だったら
+# 同じ事を一つ少ない数字でやった結果とその数字をかけたものを返すんだよ。
+#
+# define 階乗
+#
+#
+# 5が欲しいっていうのは、
+# 5を返すんだよ。
+#
+# define 5が欲しい
+# 5
+#
+# [[:DEF, "def"], [:IDENTIFIER, "5が欲しい"],
+#
+
+
 class LexerTest < MiniTest::Test
   def test_define
     assert_equal [[:DEF, "def"], [:IDENTIFIER, "名前"]], Lexer.new.tokenize("名前っていうのは")
+  end
+
+  def test_define2
+    code = <<-CODE
+掛け算っていうのは、
+数字1と数字2を使って、
+数字1と数字2をかけた結果を返すんだよ。
+CODE
+    tokens = [
+      [:DEF, "def"], [:IDENTIFIER, "掛け算"],
+      [:ARGS, "arg"], [:IDENTIFIER, "数字1"], [:IDENTIFIER, "数字2"],
+      [:RETURN, "返す"], [:*, "かけた結果"], [:IDENTIFIER, "数字1"], [:IDENTIFIER, "数字2"],
+      [:TERM, "。"]
+    ]
+    assert_equal tokens, Lexer.new.tokenize(code)
+  end
+
+  def test_
+    code = <<-CODE
+      階乗っていうのは、
+      数字を使って、
+      もしその数字が0だったら1を返して、
+      それ以外だったら
+      同じ事を一つ少ない数字でやった結果とその数字をかけたものを返すんだよ。
+    CODE
+    assert_equal [],
+      Lexer.new.tokenize(code)
   end
 end
